14743372543b489cdbea0c61bff02dab4087cd93
Extreme_ <1716224950@qq.com>
2022-10-15 20:34:55 +0800

    读取json文件问题带修复

--- soe-corpus/src/main/java/net/ecnu/service/impl/CommonServiceImpl.java
+++ soe-corpus/src/main/java/net/ecnu/service/impl/CommonServiceImpl.java
@@ -35,20 +35,16 @@ public class CommonServiceImpl implements CommonService {
 
     @Override
     public Object listMotherTongue() {
-        try {
-//            File file = ResourceUtils.getFile("classpath:static/MotherTongueList.json");
-            ClassPathResource classPathResource = new ClassPathResource("static/MotherTongueList.json");
-            File file = classPathResource.getFile();
-            String content = FileUtils.readFileToString(file, "UTF-8");
-            List<Object> languages = JSON.parseArray(content);
-            //处理返回结果
-            Map<String, Object> result = new HashMap<>(2);
-            result.put("size", languages.size());
-            result.put("languages", languages);
-            return result;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        //            File file = ResourceUtils.getFile("classpath:static/MotherTongueList.json");
+//            ClassPathResource classPathResource = new ClassPathResource("static/MotherTongueList.json");
+//            File file = classPathResource.getFile();
+//            String content = FileUtils.readFileToString(file, "UTF-8");
+        String content = "[{\"id\":1,\"motherTongue\":\"汉语\"},{\"id\":2,\"motherTongue\":\"英语\"},{\"id\":3,\"motherTongue\":\"俄语\"},{\"id\":4,\"motherTongue\":\"德语\"},{\"id\":5,\"motherTongue\":\"法语\"}]";
+        List<Object> languages = JSON.parseArray(content);
+        //处理返回结果
+        Map<String, Object> result = new HashMap<>(2);
+        result.put("size", languages.size());
+        result.put("languages", languages);
+        return result;
     }
 }
