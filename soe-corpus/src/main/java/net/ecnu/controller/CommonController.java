fb6e51d983a73865215341c4deefb644c0edbab9
Tangguoxing <1433381534@qq.com>
2022-11-04 19:25:58 +0800

    母语列表接口完善

--- soe-corpus/src/main/java/net/ecnu/controller/CommonController.java
+++ soe-corpus/src/main/java/net/ecnu/controller/CommonController.java
@@ -2,6 +2,7 @@ package net.ecnu.controller;
 
 
 import net.ecnu.service.CommonService;
+import net.ecnu.service.CommonService2;
 import net.ecnu.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +18,18 @@ public class CommonController {
     @Autowired
     private CommonService commonService;
 
+    @Autowired
+    private CommonService2 commonService2;
+
     @GetMapping("languages")
-    public JsonData languages() {
+    public JsonData languages() throws IOException {
         Object data = commonService.listMotherTongue();
+        return JsonData.buildSuccess(data);
+    }
+
+    @GetMapping("languages2")
+    public JsonData languages2() throws IOException {
+        Object data = commonService2.listMotherTongue();
         return JsonData.buildSuccess(data);
     }
 
---
+++ soe-corpus/src/main/java/net/ecnu/service/CommonService2.java
@@ -0,0 +1,8 @@
+package net.ecnu.service;
+
+
+import java.io.IOException;
+
+public interface CommonService2 {
+    Object listMotherTongue() throws IOException;
+}
