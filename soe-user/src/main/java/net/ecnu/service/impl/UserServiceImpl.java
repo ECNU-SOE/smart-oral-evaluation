831f39af0b312174d269a4518d984f167ee56d2e
Tangguoxing <1433381534@qq.com>
2022-12-06 16:51:07 +0800

    新增阿里云短信服务

--- soe-user/src/main/java/net/ecnu/service/impl/UserServiceImpl.java
+++ soe-user/src/main/java/net/ecnu/service/impl/UserServiceImpl.java
@@ -1,5 +1,13 @@
 package net.ecnu.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
 import lombok.extern.slf4j.Slf4j;
 import net.ecnu.controller.request.UserReq;
 import net.ecnu.enums.BizCodeEnum;
@@ -19,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 
 @Service
@@ -97,6 +106,33 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(userDTO,userDO);
         userDO.setAccountNo(user.getAccountNo());
         return userMapper.updateById(userDO);
+    }
+
+    @Override
+    public boolean send(String phoneNum, String templateCode, Map<String, Object> code) {
+        //连接阿里云
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tKC7ayyoZA81SE62bpH", "0v3aCE7DZyXXueE4Ui2BC8fwIVEOMj");
+        IAcsClient client = new DefaultAcsClient(profile);
+        //构建请求
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");//不要动
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        //自定义请求参数
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", phoneNum);
+        final String signName = "唐国兴的博客"; //短信签名，需要向阿里云申请
+        request.putQueryParameter("SignName",signName);    //短信签名
+        request.putQueryParameter("TemplateCode", templateCode);//短信模板code
+        request.putQueryParameter("TemplateParam", JSONObject.toJSONString(code));
+        try {
+            CommonResponse resp = client.getCommonResponse(request);// 发送短信
+            System.out.println(resp.getData());
+            return resp.getHttpResponse().isSuccess();
+        } catch (ClientException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
