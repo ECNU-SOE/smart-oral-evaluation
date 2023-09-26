894caffd8d878e45d5eae1189eb3f4aa2c32262c
Tangguoxing <1433381534@qq.com>
2023-09-26 14:20:48 +0800

    日期比较

--- soe-common/src/main/java/net/ecnu/model/CpsrcdDO.java
+++ soe-common/src/main/java/net/ecnu/model/CpsrcdDO.java
@@ -27,7 +27,7 @@ public class CpsrcdDO implements Serializable {
     /**
      * 主键id
      */
-      @TableId(value = "id", type = IdType.AUTO)
+      @TableId(value = "id", type = IdType.NONE)
     private String id;
 
     /**
